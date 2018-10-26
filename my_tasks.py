a022649a6b7fd364c461f7ea06ded88ab8208722
sergej-i <Ivakin.Sergej@gmail.com>
2018-10-26 13:49:53 +0300

    step 1&2

---
+++ my_tasks.py
@@ -0,0 +1,31 @@
+# file: my_tasks.py
+from ya_tasks import tasks
+from functools import reduce
+
+
+@tasks.task(name='multiprint', 
+            json_schema={'type': 'object',
+                         'properties': {
+                            'msg': {'type': 'string'},
+                            'count': {'type': 'integer', 'minimum': 1}
+                            },
+                            'required': ['msg']})
+def multi_print(msg, count=10):
+    return '\n'.join(msg for _ in range(count))
+
+
+class Multiply(tasks.BaseTaskV2):
+    name = 'mult'
+    json_schema = {'type': 'object',
+                   'properties': {
+                       'operands': {'type': 'array',
+                                    'minItems': 1,
+                                    'items': {'type': 'number'}}
+                    },
+                    'required': ['operands']}
+    def run(self, operands):
+        return reduce(lambda x, y: x*y, operands)
+
+
+if __name__ == '__main__':
+    tasks.run_cli()
---
+++ ya_tasks/tasks.py
@@ -0,0 +1,129 @@
+"""
+Homework.
+Task executor.
+"""
+
+import argparse
+import json
+from functools import wraps
+import jsonschema
+
+#constant
+TASK_EXEC_FUNCTION = 'function'
+TASK_EXEC_BASE_CLASS = 'base_class'
+
+
+class TaskException(Exception):
+    """ base module exception """
+
+
+class TaskExceptionWrongParameters(TaskException):
+    """ An exception is thrown when the parameters for the task
+        are incorrectly specified. """
+
+
+class TaskStorage:
+    """ Storage for task executors """
+    tasks = {}
+
+    @classmethod
+    def add(cls, task_name, obj, type_obj, json_schema):
+        """ add task executor to storage
+
+            Attributes:
+                task_name -- task name
+                obj -- function or class
+                type_obj -- type of obj
+                json_schema -- task parameters json schema
+        """
+        if task_name is not None and type_obj in [TASK_EXEC_BASE_CLASS, TASK_EXEC_FUNCTION]:
+            cls.tasks[task_name] = (obj, type_obj, json_schema)
+
+
+    @classmethod
+    def check_schema(cls, json_params, json_schema):
+        """ validation of the specified task parameters
+
+            Attributes:
+                json_params -- task parameters in json
+                json_schema -- json schema for check
+        """
+        try:
+            jsonschema.validate(json_params, json_schema)
+        except (jsonschema.exceptions.ValidationError,
+                jsonschema.exceptions.SchemaError):
+            return False
+        return True
+
+
+class BaseTask:
+    """ base class for setting the task (for python 3.6) """
+    name = None
+    json_schema = None
+
+    def __init_subclass__(cls):
+        super().__init_subclass__()
+        TaskStorage.add(cls.name, cls, TASK_EXEC_BASE_CLASS, cls.json_schema)
+
+
+class BaseTaskMeta(type):
+    """ metaclass for registering a task defined by class
+        (for python < 3.6 model)
+    """
+    name = None
+    json_schema = None
+
+    def __new__(cls, name, bases, dct):
+        subcls = super(BaseTaskMeta, cls).__new__(cls, name, bases, dct)
+        TaskStorage.add(subcls.name, subcls, TASK_EXEC_BASE_CLASS, subcls.json_schema)
+        return subcls
+
+
+class BaseTaskV2(metaclass=BaseTaskMeta):
+    """ base class for setting the task (for python < 3.6) """
+
+
+def task(name, json_schema):
+    """ decorator for registering a task defined by function """
+    def decor(func):
+        @wraps(func)
+        def wrap(*args, **kwargs):
+            return func(*args, **kwargs)
+
+        TaskStorage.add(name, wrap, TASK_EXEC_FUNCTION, json_schema)
+        return wrap
+    return decor
+
+
+def get_cmd_args():
+    """ get arguments from command line """
+    parser = argparse.ArgumentParser(description='some task executor')
+    parser.add_argument('task_name',
+                        type=str,
+                        help='task name')
+    parser.add_argument('-p', '--params',
+                        type=str,
+                        required=True,
+                        help='data for task in json')
+    try:
+        args = parser.parse_args()
+        args.params = json.loads(args.params)
+    except ():
+        raise TaskExceptionWrongParameters
+    return args
+
+
+def run_cli():
+    """ perform a task using command line parameters """
+    args = get_cmd_args()
+    if args.task_name in TaskStorage.tasks.keys():
+        task_executor, task_type, task_schema = TaskStorage.tasks[args.task_name]
+        if TaskStorage.check_schema(args.params, task_schema):
+            if task_type == TASK_EXEC_BASE_CLASS:
+                print(task_executor().run(**args.params))
+            elif task_type == TASK_EXEC_FUNCTION:
+                print(task_executor(**args.params))
+
+
+if __name__ == '__main__':
+    run_cli()
